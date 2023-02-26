ae96699a2854f058e9ed62bd8f38abf7db100373
Fezz <jonnicwolf@gmail.com>
2023-02-26 01:16:44 -0500

    housekeeping

--- src/components/NameCard.jsx
+++ src/components/NameCard.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
+import '../assets/fonts/fonts.css';
+import styled from 'styled-components';
 
 const NameCard = () => {
   return (
     <section>
-      <div>Fezz</div>
-      <div>Full Stack Web Developer / Mixologist</div>
-      <div>Freelancer, previously SeatGeek Commerce Marketplace Engineering</div>
-      <div>Based in New York City</div>
-      
+      <RampartOneText>FEZZ</RampartOneText>
+      <JuliusSansOneText>
+        {'FULL STACK DEV / MIXOLOGIST'
+          .split('')
+          .map(letter=> <span style={{letterSpacing: '0.20em'}}>{letter}</span>
+        )}
+      </JuliusSansOneText>
+      <JuliusSansOneText>
+        {'Freelancer, previously SeatGeek Commerce Marketplace Engineering'
+          .toUpperCase()
+          .split('')
+          .map(letter=> <span style={{letterSpacing: '0.0250em', fontSize: '18px'}}>{letter}</span>
+        )}
+      </JuliusSansOneText>
+      <JuliusSansOneText>
+        <span style={{fontSize: '18px'}}>
+          BASED IN NEW YORK CITY
+        </span>
+      </JuliusSansOneText>
     </section>
   )
 };
 
-export default NameCard; 
+const ClimateCrisisText = styled.p`
+  font-family: var(--font-family-climate-crisis);
+  font-weight: var(--font-weight-normal);
+`
+const JuliusSansOneText = styled.p`
+  font-family: var(--font-family-julius-sans-one);
+  font-weight: var(--font-weight-normal);
+  font-size: 25px;
+`
+const RampartOneText = styled.p`
+  font-family: var(--font-family-rampart-one);
+  font-weight: var(--font-weight-normal);
+  font-size: 50px;
+`
+export default NameCard;
