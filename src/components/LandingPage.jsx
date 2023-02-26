059553e87bad5192114256b8dcc0b0027f4f6dc6
Fezz <jonnicwolf@gmail.com>
2023-02-25 20:49:48 -0500

    scaffolding for prolinks

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NameCard from 'src/components/sections/NameCard'
+
+const LandingPage = () => {
+  return (
+    <div>
+      <div>
+        <NameCard />
+        <ProLinks />
+      </div>
+    </div>
+  )
+};
+
+export default LandingPage;
