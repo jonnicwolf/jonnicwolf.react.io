66fe811edc6e22fdb3819ff1d539969b13bb88ca
Fezz <jonnicwolf@gmail.com>
2023-02-25 20:57:35 -0500

    add info to name card

--- src/components/NameCard.jsx
+++ src/components/NameCard.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const NameCard = () => {
   return (
     <section>
-      <div>Name</div>
+      <div>Fezz</div>
+      <div>Full Stack Web Developer / Mixologist</div>
+      <div>Freelancer, previously SeatGeek Commerce Marketplace Engineering</div>
+      <div>Based in New York City</div>
+      
     </section>
   )
 };
