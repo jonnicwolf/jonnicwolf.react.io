67800096b1330cfd0c6c0ad0ee4cc1c3750ff6b3
Fezz <jonnicwolf@gmail.com>
2023-02-27 04:24:40 -0500

    background set

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import NameCard from '../components/NameCard.jsx';
 import ProLinks from './ProLinks';
 
 const LandingPage = () => {
   return (
-    <div>
-      <div>
-        <NameCard />
-        <br /><br /><br /><br /><br />
-        <span>Space for p5js project</span>
-        <ProLinks />
-        <button>Enter</button>
-      </div>
-    </div>
+    <Background>
+      <NameCard />
+      <br /><br /><br /><br /><br />
+      <span>Space for p5js project</span>
+      <ProLinks />
+      <button>Enter</button>
+    </Background>
   )
 };
+
+const Background = styled.div`
+  background-image: radial-gradient(circle 1550px, white, rgb(42, 191, 250)); 
+  opacity: 100%;
+  height: 1000px;
+`
 
 export default LandingPage;
