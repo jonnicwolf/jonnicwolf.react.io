500d714116083650535ffdd752aabb6b9ab00061
Fezz <jonnicwolf@gmail.com>
2024-04-09 17:03:06 -0400

    adds lorenz to project page

--- src/components/p5/P5_LORENZ_ATTRACTOR.jsx
+++ src/components/p5/P5_LORENZ_ATTRACTOR.jsx
@@ -36,8 +36,9 @@ const P5_LORENZ_ATTRACTOR = () => {
       // Plotted coordinates are stored as an array of objects
       points.push(new p5.Vector(x, y, z));
   
-      p.scale(10);
+      p.scale(15);
       p.strokeWeight(3);
+      p.translate(0, 0)
       p.noFill();
   
       p.beginShape();
