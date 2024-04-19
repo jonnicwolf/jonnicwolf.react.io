abfa30bbe1af29560c0c766a0d4d235063d37b42
Fezz <jonnicwolf@gmail.com>
2024-04-19 15:19:32 -0400

    p5s hooked up

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -1,27 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-import Button from '../assets/buttons/Button.jsx'
+import Button from '../assets/buttons/Button.jsx';
+import P5_LORENZ_ATTRACTOR from '../components/p5/P5_LORENZ_ATTRACTOR.jsx';
+import P5_PLANE from '../components/p5/P5_PLANE.jsx';
+import P5_TORUS from '../components/p5/P5_TORUS.jsx';
+
 
 const GalleryPage = ({ darkMode }) => {
+  const [index, setIndex] = useState(0);
+  const [title, setTitle] = useState('TORUS');
+  const p5_projs = [ Torus, Plane, Lorenz ];
+  
+  const handleNext = () => setIndex( index >= p5_projs.length - 1 ? 0 : index++ );
+  const handleLast = () => setIndex( index <= 1 ? p5_projs.length - 1 : index--);
+
+  const ActiveProject = p5_projs[index];
+
   return (
     <Container>
       <DisplayBox>
         <ControlBox>
           <Last text={'LAST'}/>
-          <Title>TITLasdfasdfadsfadsfadfadsfaE</Title>
+          <Title>{title}</Title>
           <Next text={'NEXT'}/>
         </ControlBox>
+        <ActiveProject />
       </DisplayBox>
     </Container>
   );
 };
 
 const Container = styled.div`
-
+  border: 1px solid red;
 `;
 const Title = styled.h3`
-  border: 1px solid red;
   height: 100%;
   display: flex;
   font-family: var(--font-family-rubik);
@@ -37,15 +50,16 @@ const ControlBox = styled.div`
   float: right;
   justify-content: space-evenly;
   align-items: center;
-  margin: 15px;
+  margin: 20px;
   border: 1px solid grey;
-  width: 50%;
+  width: 30%;
 `;
-const Next = styled(Button)`
 
-`;
-const Last = styled(Button)`
-  
-`;
+const Next   = styled(Button)``;
+const Last   = styled(Button)``;
+
+const Torus  = styled(P5_TORUS)``;
+const Lorenz = styled(P5_LORENZ_ATTRACTOR)``;
+const Plane  = styled(P5_PLANE)``;
 
 export default GalleryPage;
