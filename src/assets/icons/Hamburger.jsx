36ba6f256ebce2cf131932139c19bb5c2b54e1a8
Fezz <jonnicwolf@gmail.com>
2024-04-22 23:21:28 -0400

    housekeeping

---
+++ src/assets/icons/Hamburger.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Hamburger = ({ darkMode }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle () {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  };
+
+  return (
+    <Wrapper>
+      {
+        isOpen ?
+          <Container onClick={()=>handleToggle()}>
+            <Bar />
+            <Bar />
+            <Bar />
+          </Container>
+        :
+         <XContainer onClick={()=>handleToggle()}>
+          <X_1/>
+          <X_2/>
+         </XContainer>
+      }
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 45px;
+  height: 30px;
+`;
+const Container = styled.div`
+  cursor: pointer;
+  width: 30px;
+  height: 25px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  transition: all .3s linear;
+`;
+const XContainer = styled(Container)`
+  justify-content: center;
+  padding-left: 10px;
+`;
+const Bar = styled.span`
+  display: block;
+  width: 100%;
+  height: 3px;
+  background-color: black;
+`;
+const X_1 = styled(Bar)`
+  transform: rotate(45deg);
+  transform-origin: top left;
+  margin-bottom: 8px;
+`;
+const X_2 = styled(Bar)`
+  transform: rotate(-45deg);
+  transform-origin: bottom left;
+  margin-top: 9px;
+`;
+
+export default Hamburger;
