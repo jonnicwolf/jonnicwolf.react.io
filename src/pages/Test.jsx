4d358d9037a572350c2861e63a2d315dec7e9c1c
Fezz <jonnicwolf@gmail.com>
2024-04-24 01:25:11 -0400

    adds test to gitignore

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Test = () => {
   return (
-    <></>
+    <Container>
+      WELL HELLO MOTHERFUCKER, NOTHING TO SEE HERE
+    </Container>
   );
 };
+
+const Container = styled.div`
+  margin-top: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: Rubik;
+  font-weight: bold;
+  font-size: 2em;
+  height: 70vh;
+`;
 
 export default Test;
