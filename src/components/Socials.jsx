30373bee269cd1654516fb3d9c800ac20a04cf7b
Fezz <jonnicwolf@gmail.com>
2024-05-04 04:40:39 -0400

    refactors social icons

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -5,38 +5,24 @@ import styled from 'styled-components';
 const Socials = () => {
   return (
     <SocialsList>
-        {/* SocialIcons are from an external library, therefore inline or stylesheet styles are necessary. */}
         <SocialIcon
           url='https://www.linkedin.com/in/jonathannarine/'
-          bgColor='white'
-          fgColor='rgb(130,216,255)'
-          style={{border: '1px solid black', borderRadius: '25px'}}
-        />
+          target='_blank' />
         <SocialIcon
           url='https://github.com/jonnicwolf'
-          bgColor='white'
-          fgColor='rgb(130,216,255)'
-          style={{border: '1px solid black', borderRadius: '25px'}}
-        />
+          target='_blank' />
         <SocialIcon
           url='mailto:jonnciwolf@gmail.com'
-          bgColor='white'
-          fgColor='rgb(130,216,255)'
-          style={{border: '1px solid black', borderRadius: '25px'}}
-        />
+          target='_blank' />
         <SocialIcon
-          url='https://docs.google.com/viewer?url=https://docs.google.com/document/d/1L--uuFkBDl2thpDIuyzyUWNqv5Q0BShsKrwGvy6DusU/export?format=pdf'
-          bgColor='white'
-          fgColor='rgb(130,216,255)'
-          style={{border: '1px solid black', borderRadius: '25px'}}
-          target='_blank'
-        />
+          url='https://docs.google.com/viewer?url=https://docs.google.com/document/d/1L--uuFkBDl2thpDIuyzyUWNqv5Q0BShsKrwGvy6DusU/export?format=pdf' 
+          target='_blank' />
     </SocialsList>
-  )
+  );
 };
 
 
-const SocialsList = styled.div`  
+const SocialsList = styled.div`
   column-gap: 10px;
   display: flex;
   padding-inline-start: 0px;
