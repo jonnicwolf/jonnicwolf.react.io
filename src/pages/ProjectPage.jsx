4243c94cde19111dd598fb91967456605109bb18
Fezz <jonnicwolf@gmail.com>
2024-07-02 03:05:42 -0400

    removes framer

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -33,7 +33,7 @@ export default function ProjectPage () {
     framer
   ] = [ 'devicon-javascript-plain', "devicon-react-original", "devicon-p5js-original", "devicon-firebase-plain-wordmark", "devicon-postgresql-plain", "devicon-css3-plain-wordmark", "devicon-framermotion-original-wordmark colored"];
 
-  const hotc_devi = [javascript, react, p5, firebase, framer];
+  const hotc_devi = [javascript, react, p5, firebase];
   const scd_devi= [javascript, react, postgresql, firebase];
   const ttp_devi = [javascript, react, css3];
 
