a40b7fe210ab170fe0b505af7c4fab3c2020fb7a
Fezz <jonnicwolf@gmail.com>
2024-07-03 01:26:48 -0400

    Changes height for App to fix uneven background cover

--- src/App.js
+++ src/App.js
@@ -43,8 +43,7 @@ function App() {
 const Container = styled.div`
   overflow-x: hidden;
   background: ${props => props.darkMode ? '#5c5c5b' : '#fffcf5'};
-  height: ${props => props.isMobile ? '150vh' : '250vh'};
-  height: 100%;
+  height: ${props => props.isMobile ? '250vh' : '250vh'};
 `;
 
 export default App;
