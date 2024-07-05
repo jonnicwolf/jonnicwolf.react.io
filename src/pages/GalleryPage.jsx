2bc92bb80d2c0739c79ea99709d50ac7cce4c257
Fezz <jonnicwolf@gmail.com>
2024-07-05 04:13:50 -0400

    "Updated GalleryPage component: added useMemo hook, renamed useParams variable, updated useEffect and navigation logic, and refactored handleNext and handleLast functions."

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -11,47 +11,47 @@ import P5_ANJA from '../components/p5/P5_ANJA.jsx';
 import P5_GEOSTORM from '../components/p5/P5_GEOSTORM.jsx';
 
 const GalleryPage = ({ darkMode }) => {
-  const { project } = useParams();
+  const { projectName } = useParams();
   const navigate = useNavigate();
   const [index, setIndex] = useState(0);
-  const [title, setTitle] = useState('');
-  const p5_projs = [
-    // { component: P5_ , title: '', name: '' },
+  const p5_projs = useMemo(() => [
     { component: P5_TORUS, title: 'TORUS', name: 'torus' },
     { component: P5_PLANE, title: 'HORIZON', name: 'horizon' },
     { component: P5_LORENZ_ATTRACTOR, title: 'CHAOS THEORY', name: 'chaos-theory' },
+    { component: P5_LOADER, title: 'AFTERIMAGE', name: 'afterimage' },
     { component: P5_ANJA, title: 'ANJA', name: 'anja' },
     { component: P5_GEOSTORM, title: 'TRIGON SQUALL', name: 'trigon-squall' },
-  ];
+  ], []);
 
   useEffect(() => {
-    const index = p5_projs.findIndex( proj => proj.name === project);
-    if (index >= 0) {
-      setIndex(index);
-      setTitle(p5_projs[index].title);
+    const param_index = p5_projs.findIndex(item => item.name === projectName);
+    if (param_index >= 0) {
+      setIndex(param_index);
     } else {
-      navigate('/gallery/torus');
-    };
-  }, [index]);
+      navigate(`/gallery/${p5_projs[0].name}`);
+    }
+  }, [projectName, p5_projs, navigate]);
 
-  const handleNext = (index) => {
-    setIndex(index >= p5_projs.length - 1 ? 0 : index + 1);
-    navigate(`/gallery/${p5_projs[index].name}`);
+  const handleNext = () => {
+    const nextIndex = (index + 1) % p5_projs.length;
+    navigate(`/gallery/${p5_projs[nextIndex].name}`);
   };
-  const handleLast = (index) => {
-    setIndex(index <= 0 ? p5_projs.length - 1 : index - 1);
-    navigate(`/gallery/${p5_projs[index].name}`);
+
+  const handleLast = () => {
+    const prevIndex = (index - 1 + p5_projs.length) % p5_projs.length;
+    navigate(`/gallery/${p5_projs[prevIndex].name}`);
   };
 
   const ActiveProject = p5_projs[index].component;
+  const projectTitle = p5_projs[index].title;
 
   return (
     <Container>
       <DisplayBox>
         <ControlBox darkMode={darkMode}>
-          <Button text={'LAST'} onclick={ () => handleLast(index) } darkModeGetter={ darkMode } />
-          <Title>{ title }</Title>
-          <Button text={'NEXT'} onclick={ () => handleNext(index) } darkModeGetter={ darkMode } />
+          <Button text={'LAST'} onclick={handleLast} darkModeGetter={darkMode} />
+          <Title>{projectTitle}</Title>
+          <Button text={'NEXT'} onclick={handleNext} darkModeGetter={darkMode} />
         </ControlBox>
         <ActiveProject strokeColor={200} darkMode={darkMode} />
       </DisplayBox>
@@ -64,6 +64,7 @@ const Container = styled.div`
   overflow: hidden;
   height: 100vh;
 `;
+
 const Title = styled.h2`
   height: 100%;
   display: flex;
@@ -72,6 +73,7 @@ const Title = styled.h2`
   font-weight: bold;
   font-family: 'Rubik', sans-serif;
 `;
+
 const DisplayBox = styled.div`
   position: fixed;
   height: 100vh;
@@ -81,6 +83,7 @@ const DisplayBox = styled.div`
   align-items: center;
   overflow: hidden;
 `;
+
 const ControlBox = styled.div`
   display: flex;
   justify-content: space-evenly;
@@ -88,9 +91,9 @@ const ControlBox = styled.div`
   margin: 20px 10px 0 10px;
   width: 30%;
   text-wrap: wrap;
-  color:            ${props => props.darkMode ? 'white' : 'black'};
-  background-color: ${props => props.darkMode ? '150'   : null};
-  font-color:       ${props => props.darkMode ? 'grey'  : 'white'};
+  color: ${props => (props.darkMode ? 'white' : 'black')};
+  background-color: ${props => (props.darkMode ? '150' : null)};
+  font-color: ${props => (props.darkMode ? 'grey' : 'white')};
   @media screen and (max-width: 1024px) {
     margin-top: 6vh;
     gap: 5vw;
