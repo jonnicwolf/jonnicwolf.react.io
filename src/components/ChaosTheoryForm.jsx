3ce686a030c61f5649cfec1ff5b0bee1282955bd
Fezz <jonnicwolf@gmail.com>
2024-07-12 03:23:54 -0400

    "Refactor ChaosTheoryForm component: add Info element, reorganize VarWrap elements, and update styles for Container, Button, Var, and new VarContainer and Info components."

--- src/components/ChaosTheoryForm.jsx
+++ src/components/ChaosTheoryForm.jsx
@@ -31,33 +31,38 @@ export default function ChaosTheoryForm({ vars, setters, darkMode }) {
 
   return (
     <Container>
-      <VarWrap>
-        <Button onClick={handleSigmaUp}>⬆</Button>
-        <Var>𝛴 {sigma}</Var>
-        <Button onClick={handleSigmaDown}>⬇</Button>
-      </VarWrap>
-      <VarWrap>
-        <Button onClick={handleRhoUp}>⬆</Button>
-        <Var>⍴ {rho}</Var>
-        <Button onClick={handleRhoDown}>⬇</Button>
-      </VarWrap>
-      <VarWrap>
-        <Button onClick={handleBetaUp}>⬆</Button>
-        <Var>β {Math.floor(beta)}</Var>
-        <Button onClick={handleBetaDown}>⬇</Button>
-      </VarWrap>
+      <Info>Each configuration is a unique infinite loop that eventually converges to the same butterfly shape</Info>
+
+      <VarContainer>
+        <VarWrap>
+          <Button onClick={handleSigmaUp} onMouseDown={handleSigmaUp}>⬆</Button>
+          <Var>𝛴 {sigma}</Var>
+          <Button onClick={handleSigmaDown} onMouseDown={handleSigmaDown}>⬇</Button>
+        </VarWrap>
+        <VarWrap>
+          <Button onClick={handleRhoUp} onMouseDown={handleRhoUp}>⬆</Button>
+          <Var>⍴ {rho}</Var>
+          <Button onClick={handleRhoDown} onMouseDown={handleRhoDown}>⬇</Button>
+        </VarWrap>
+        <VarWrap>
+          <Button onClick={handleBetaUp} onMouseDown={handleBetaUp}>⬆</Button>
+          <Var>β {Math.floor(beta)}</Var>
+          <Button onClick={handleBetaDown} onMouseDown={handleBetaDown}>⬇</Button>
+        </VarWrap>
+      </VarContainer>
+
     </Container>
   );
 };
 
 const Container = styled.div`
   border: 1px solid grey;
-  padding: 25px 80px;
   display: flex;
-  justify-content: center;
-  gap: 8vw;
+  flex-direction: column;
+  gap: 15px;
   margin-top: 15px;
   width: 40vw;
+  padding: 10px 40px;
 `;
 const Button = styled.button`
   border: none;
@@ -71,6 +76,17 @@ const Var = styled.div`
 const VarWrap = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
   justify-content: center;
+`;
+const Info = styled.div`
+  font-family: Rubik;
+  font-size: 1rem;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+`;
+const VarContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 5vw;
 `;
