8bf5d4fcc6a21feba1fd58baf6cc4359494708b0
Fezz <jonnicwolf@gmail.com>
2024-08-01 02:14:29 -0400

    "Updated EsperiCover component with new styles and layout"

--- src/components/EsperiCover.jsx
+++ src/components/EsperiCover.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Eye from './Eye.jsx';
 
 export default function EsperiCover() {
   return (
-    <>
-      <Eye width={100} height={280} />
-    </>
+    <Container>
+      <EyeWrap>
+        <Eye width={100} height={280} />
+      </EyeWrap>
+      <Title>Esperi</Title>
+    </Container>
   );
 };
 
 const Container = styled.div`
-  border: 1px solid red;
+  background-color: rgb(252, 230, 197);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  height: 700px;
+  width: 800px;
+
+  --s: 5px;
+  padding: var(--s);
+  border: calc(5*var(--s)) solid #0000;
+  outline: 1px solid #000;
+  outline-offset: calc(-3*var(--s));
+  background: conic-gradient(from 90deg at 1px 1px,#0000 25%,#000 0);
+  background-color: #fce6c5;
+`;
+
+const EyeWrap = styled.div`
+  padding: 50px;
+  transform: translateX(-20px);
 `
+const Title = styled.h1`
+  font-size: 8rem;
+  font-family: Bagnard;
+`;
