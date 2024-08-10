9c5bd805d778ac40071c2c9942d4291c1df97639
Fezz <jonnicwolf@gmail.com>
2024-08-10 08:07:39 -0400

    "Updated ContactMe component: replaced SocialIcon with Socials, added Background and Title elements, and modified styles"

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { SocialIcon } from 'react-social-icons';
+import Socials from './Socials';
 
 export default function ContactMe() {
   return (
     <Container>
-      <>
-        <SocialIcon url='' />
-      </>
+      <Background/>
+      
+      <Title>Contact Me</Title>
+      
+      <Socials gap={'10px'}/>
     </Container>
   );
 };
 
 const Container = styled.div`
-
+  display: flex;
 `;
+const Background = styled.div`
+  background: repeat url('https://img.icons8.com/?size=100&id=iZfvYzdS1M3T&format=png&color=000000');
+  width: 50%;
+  height: 800px;
+  width: 400px;
+  opacity: 0.2
+`;
+const Title = styled.h1`
+  font-family: Michroma;
+`;
+
