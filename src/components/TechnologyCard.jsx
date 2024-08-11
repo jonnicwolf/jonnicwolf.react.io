135b82bf58a8e61509cf8fd0336af751bdb07ee1
Fezz <jonnicwolf@gmail.com>
2024-08-11 10:01:18 -0400

    "Removed LinkButton component, replaced 'i' tags with 'Icon' component, replaced 'img' tags with 'SVG' component, and added styled components for Icon and SVG"

--- src/components/TechnologyCard.jsx
+++ src/components/TechnologyCard.jsx
@@ -16,10 +16,6 @@ export default function TechnologyCard () {
           isScaleAnimation={true} />
         <LinkButton
           // onClick={handleClick_more}
-          textContent='TECH'
-          isScaleAnimation={true} />
-        <LinkButton
-          // onClick={handleClick_more}
           textContent='OFFLINE'
           isScaleAnimation={true} />
         <LinkButton
@@ -32,63 +28,65 @@ export default function TechnologyCard () {
         <TechGroup>
           <IntroText fontSize='1.3rem'>LANGUAGES</IntroText>
           <TechContent>
-            <img style={{height: '5rem'}} src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" title='JavaScript'/>
-            <img style={{height: '5rem'}} src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original-wordmark.svg" title='Python' />
-            <i className='devicon-swift-plain colored' title='Swift' />
-            <i className='devicon-typescript-plain colored' title='TypeScript' />
+            <SVG style={{height: '5rem'}} src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" title='JavaScript'/>
+            <SVG style={{height: '5rem'}} src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original-wordmark.svg" title='Python' />
+            <Icon className='devicon-swift-plain colored' title='Swift' />
+            <Icon className='devicon-typescript-plain colored' title='TypeScript' />
           </TechContent>
         </TechGroup>
 
         <TechGroup>
           <IntroText fontSize='1.3rem'>STYLING</IntroText>
           <TechContent>
-            <StyledComponents title='Styled-Components'>
-              <StyledIcon>{'<💅>'}</StyledIcon>
-              <StyledName>STYLED COMPONENTS</StyledName>
-            </StyledComponents>
-            <i className='devicon-p5js-original colored' title='p5.js' />
-            <i className='devicon-css3-plain colored' title='CSS3' />
-            <i className='devicon-bootstrap-plain colored' title='Bootstrap' />
-            <i className='devicon-sass-plain colored' title='Sass' />
+            
+              <StyledComponents title='Styled-Components'>
+                <StyledIcon>{'<💅>'}</StyledIcon>
+                <StyledName>STYLED COMPONENTS</StyledName>
+              </StyledComponents>
+            
+            <Icon className='devicon-p5js-original colored' title='p5.js' />
+            <Icon className='devicon-css3-plain colored' title='CSS3' />
+            <Icon className='devicon-bootstrap-plain colored' title='Bootstrap' />
+            <Icon className='devicon-sass-plain colored' title='Sass' />
           </TechContent>
         </TechGroup>
 
         <TechGroup>
           <IntroText fontSize='1.3rem'>LIBRARIES</IntroText>
           <TechContent>
-            <i className='devicon-react-original-wordmark colored' title='React' />
-            <i className='devicon-redux-original colored' title='Redux' />
-            <i className='devicon-axios-plain colored' title='Axios' />
-            <i className='devicon-nodejs-plain colored' title='Node.js' />
-            <i className='devicon-express-original colored' title='Express.js' />
-            <i className='devicon-firebase-plain colored' title='Firebase' />
-            <i className='devicon-sentry-plain colored' title='Sentry' />
-            <i className='devicon-cypressio-plain colored' title='Cypress.io' />
-            <i className='devicon-jest-plain colored' title='Jest' />
-            <i className='devicon-git-plain colored' title='Git' />
-            <i className='devicon-npm-original-wordmark colored' title='NPM Package Manager' />
+            <Icon className='devicon-react-original-wordmark colored' title='React' />
+            <Icon className='devicon-redux-original colored' title='Redux' />
+            <Icon className='devicon-axios-plain colored' title='Axios' />
+            <Icon className='devicon-nodejs-plain colored' title='Node.js' />
+            <Icon className='devicon-express-original colored' title='Express.js' />
+            <Icon className='devicon-firebase-plain colored' title='Firebase' />
+            <Icon className='devicon-sentry-plain colored' title='Sentry' />
+            <Icon className='devicon-cypressio-plain colored' title='Cypress.io' />
+            <Icon className='devicon-jest-plain colored' title='Jest' />
+            <Icon className='devicon-git-plain colored' title='Git' />
+            <Icon className='devicon-npm-original-wordmark colored' title='NPM Package Manager' />
           </TechContent>
         </TechGroup>
 
         <TechGroup>
           <IntroText fontSize='1.3rem'>TOOLS</IntroText>
           <TechContent>
-            <i className='devicon-docker-plain colored' title='Docker' />
-            <i className='devicon-rabbitmq-plain colored' title='RabbitMQ' />
-            <i className='devicon-terraform-plain colored' title='Terraform' />
-            <i className='devicon-kubernetes-plain colored' title='Kubernetes' />
-            <i className='devicon-jenkins-plain colored' title='Jenkins' />
-            <i className='devicon-postgresql-plain colored' title='PostgreSQL' />
-            <i className='devicon-datagrip-plain colored' title='DataGrip' />
-            <i className='devicon-postman-plain colored' title='Postman' />
-            <i className='devicon-github-plain colored' title='GitHub' />
-            <i className='devicon-gitlab-plain colored' title='Gitlab' />
-            <i className='devicon-vscode-plain colored' title='Visual Studio Code' />
-            <i className='devicon-jira-plain  colored' title='Jira' />
-            <i className='devicon-curl-plain  colored' title='cURL' />
-            <img style={{ height: '5rem'}} src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/slack/slack-original.svg" title='Slack' />
-            <img style={{ height: '5rem'}} src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" title='Figma' />
-            <i className='devicon-trello-plain colored' title='Trello' />
+            <Icon className='devicon-docker-plain colored' title='Docker' />
+            <Icon className='devicon-rabbitmq-plain colored' title='RabbitMQ' />
+            <Icon className='devicon-terraform-plain colored' title='Terraform' />
+            <Icon className='devicon-kubernetes-plain colored' title='Kubernetes' />
+            <Icon className='devicon-jenkins-plain colored' title='Jenkins' />
+            <Icon className='devicon-postgresql-plain colored' title='PostgreSQL' />
+            <Icon className='devicon-datagrip-plain colored' title='DataGrip' />
+            <Icon className='devicon-postman-plain colored' title='Postman' />
+            <Icon className='devicon-github-plain colored' title='GitHub' />
+            <Icon className='devicon-gitlab-plain colored' title='Gitlab' />
+            <Icon className='devicon-vscode-plain colored' title='Visual Studio Code' />
+            <Icon className='devicon-jira-plain  colored' title='Jira' />
+            <Icon className='devicon-curl-plain  colored' title='cURL' />
+            <SVG style={{ height: '5rem'}} src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/slack/slack-original.svg" title='Slack' />
+            <SVG style={{ height: '5rem'}} src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" title='Figma' />
+            <Icon className='devicon-trello-plain colored' title='Trello' />
           </TechContent>
         </TechGroup>
       </Content>
@@ -161,6 +159,12 @@ const Container = styled(motion.div)`
       align-items: center;
       gap: 5px;
     `;
+      const Icon = styled.i`
+        &: hover { scale: 1.1 };
+      `;
+      const SVG = styled.img`
+        &: hover { scale: 1.1 };
+      `;
   const Buttons = styled.div`
     display: flex;
     align-items: center;
