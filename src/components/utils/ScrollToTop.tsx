5a442571cec7c78d5d35b4f7618b30def6d55af8
Fezz <jonnicwolf@gmail.com>
2024-08-30 20:04:24 -0400

    "Converts ScrollToTop"

--- src/components/utils/ScrollToTop.tsx
+++ src/components/utils/ScrollToTop.tsx
@@ -1,7 +1,7 @@
-import { useEffect } from 'react';
+import { FC,useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-const ScrollToTop = () => {
+const ScrollToTop: FC = () => {
   const location = useLocation();
 
   useEffect(() => {
