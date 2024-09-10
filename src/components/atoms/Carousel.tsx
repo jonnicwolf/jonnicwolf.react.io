cc367417c3018e13225cf5eb2400d2b49d4fbc48
Fezz <jonnicwolf@gmail.com>
2024-09-10 15:12:44 -0400

    Adds Carousel and SideMenu components.

---
+++ src/components/atoms/Carousel.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+// @ts-ignore 
+import styled from 'styled-components';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Carousel: FC<Props> = ({ children }) => {
+  return (
+    <div>Carousel</div>
+  );
+};
+
+export default Carousel;
---
+++ src/components/molecules/SideMenu.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// @ts-ignore
+import styled from 'styled-components';
+
+import Button from '../../assets/buttons/Button';
+
+
+const SideMenu = () => {
+  return (
+    <div>SideMenu</div>
+  );
+};
+
+export default SideMenu;
