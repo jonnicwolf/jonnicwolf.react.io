abe26481fa2d13727646dfb896aa3127ac950860
Fezz <jonnicwolf@gmail.com>
2024-09-11 02:36:35 -0400

    "Adds DarkmodeContext to SideMenu component and updates styles to support dark mode"

--- src/components/molecules/SideMenu.tsx
+++ src/components/molecules/SideMenu.tsx
@@ -1,4 +1,5 @@
-import { FC } from 'react';
+import { FC, useContext } from 'react';
+import { DarkmodeContext } from '../../Darkmode';
 // @ts-ignore
 import styled from 'styled-components';
 
@@ -6,17 +7,17 @@ import SideMenuButton from '../atoms/SideMenuButton';
 import Carousel from '../atoms/Carousel';
 
 const SideMenu: FC = () => {
+  const { darkmode } = useContext(DarkmodeContext);
 
   const children = [
-
     <SideMenuButton img='devicon-react-original' />,
     <SideMenuButton img="devicon-blender-original" />,
     <SideMenuButton img="devicon-apple-original" />,
   ];
 
   return (
-    <Container>
-      <Current>WEB</Current>
+    <Container darkmode={darkmode}>
+      <Current darkmode={darkmode}>WEB</Current>
       <Carousel
         children={children} />
     </Container>
@@ -28,15 +29,17 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  border: 1px solid red;
   gap: 20px;
-  width: 300px;
-  height: 300px;
+  padding: 25px 40px;
+  border: 1px solid ${(props: {darkmode: boolean}) => props.darkmode ? 'white' : 'black'};
+  color: ${(props: {darkmode: boolean}) => props.darkmode ? 'white' : 'black'};
   `;
   const Current = styled.div`
   padding: 15px 60px;
   border: 1px solid red;
   font-family: Michroma;
+  border: 1px solid ${(props: {darkmode: boolean}) => props.darkmode ? 'white' : 'black'};
+  color: ${(props: {darkmode: boolean}) => props.darkmode ? 'white' : 'black'};
 `;
 
 export default SideMenu;
