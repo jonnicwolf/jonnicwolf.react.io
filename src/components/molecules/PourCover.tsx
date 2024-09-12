1d1e1ec484dc4b50155b136260878bf009524b7a
Fezz <jonnicwolf@gmail.com>
2024-09-12 00:19:06 -0400

    Creates PourCover

---
+++ src/components/molecules/PourCover.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+// @ts-ignore
+import styled from 'styled-components';
+
+const PourCover = () => {
+  return (
+    <div>PourCover</div>
+  );
+};
+
+export default PourCover;
