c8e23322a9bbdb48ead12a75bbc23ff63d083d38
Jon Narine <jon.wolf.narine@gmail.com>
2024-12-04 09:13:00 -0500

    Adds soiree to webprojects

--- src/routes/WebProjects.tsx
+++ src/routes/WebProjects.tsx
@@ -24,6 +24,11 @@ const WebProjects: FC<Props> = ({ enterClicked, setEnterClicked }) => {
     p5,
     firebase,
     postgresql,
+    sequelize,
+    express,
+    axios,
+    tailwindcss,
+    heroku,
   ] = [
     'devicon-javascript-plain',
     'devicon-typescript-plain',
@@ -32,15 +37,35 @@ const WebProjects: FC<Props> = ({ enterClicked, setEnterClicked }) => {
     'devicon-firebase-plain',
     'devicon-postgresql-plain',
     'devicon-swift-plain',
+    'devicon-sequelize-plain',
+    'devicon-express-original',
+    'devicon-axios-plain',
+    'devicon-tailwindcss-original',
+    'devicon-heroku-original',
   ];
 
   const hotc = [typescript, react, p5, firebase];
   const scd = [javascript, react, postgresql, firebase];
+  const soiree = [typescript, react, sequelize, express, axios, tailwindcss, postgresql, heroku];
 
   return (
     <Container getter={enterClicked}>
       <ProjectList getter={enterClicked}>
         <LandingPage setter={setEnterClicked} />
+
+        <ProjectCard
+          title='Soirée'
+          bio='Bar/Restaurant Reservation App'
+          liveLink='https://soiree-drab.vercel.app'
+          githubLink='https://github.com/jonnicwolf/stoned_cold_dreamery'
+          coverLink='https://res.cloudinary.com/dl7ortwgk/image/upload/v1733318854/Screenshot_2024-12-04_at_8.25.45_AM_k3znts.png'
+          // @ts-ignore
+          cover={ProjectCover}
+          gifPath='https://img-jonnicwolf.s3.us-east-2.amazonaws.com/soiree-ezgif.com-video-to-gif-converter+(1).gif'
+          apis={['BARBACKAPI']}
+          descriptions={['E-COMMERCE', 'FULL STACK']}
+          devicons={soiree}
+        />
 
         <ProjectCard
           title='Esperi'
@@ -68,6 +93,7 @@ const WebProjects: FC<Props> = ({ enterClicked, setEnterClicked }) => {
           descriptions={['E-COMMERCE', 'FULL STACK']}
           devicons={scd}
         />
+
       </ProjectList>
 
       <Background strokeColor={150} showControls={false} />
