7f1c3a6c7ce2f6ab779c952e135274b5f1343da6
Jon Narine <jon.wolf.narine@gmail.com>
2025-01-03 07:42:29 -0500

    Randomizes blink

--- src/components/p5/P5_ANJA.tsx
+++ src/components/p5/P5_ANJA.tsx
@@ -123,7 +123,8 @@ const Wrapper = styled.div`
   width: 100%;
   height: 100%;
   clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
-  animation: ${blink} 7s infinite;
+  animation-delay: ${() => Math.random() * 10}s;
+  animation: ${blink} 10s infinite;
   background-color: #000;
 `;
 const Container = styled.div`
