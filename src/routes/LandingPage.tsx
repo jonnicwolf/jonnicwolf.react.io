f5cf0c2f2fe714ef72ddbd4ef6feeadbcc1fa5dd
Jon Narine <jon.wolf.narine@gmail.com>
2025-01-08 12:09:25 -0500

    Fixes LandingPage background not covering screen on mobile

--- src/routes/LandingPage.tsx
+++ src/routes/LandingPage.tsx
@@ -51,7 +51,7 @@ const LandingPage: FC<Props> = ({ setter }) => {
 
   const backgroundAnimation = {
     hidden: {
-      height: '102vh',
+      height: '1000vh',
       width: '100vw',
       y: -100,
     },
@@ -98,12 +98,12 @@ const LandingPage: FC<Props> = ({ setter }) => {
 };
 
 const Background = styled(motion.div)<Background>`
-  background-image: radial-gradient(circle 80vh, #62c2c4, ${(props: { showAbout: boolean}) => props.showAbout ? `rgb(255,255,255)`: `rgb(17, 100, 102)` });
-  transform: translateY(${(props: { isclicked: boolean }) => props.isclicked ? '100px': '-100px'});
+  background-image: radial-gradient(circle 80vh, #62c2c4, ${props => props.showAbout ? `rgb(255,255,255)`: `rgb(17, 100, 102)` });
+  transform: translateY(${props => props.isclicked ? '100px': '-100px'});
 `;
 const LandingPageContainer = styled.div<LandingPageContainer>`
   display: flex;
-  height: 100%;
+  height: 100vh;
   align-items: center;
   justify-content: space-between;
   flex-direction: column;
@@ -128,7 +128,7 @@ const PProject = styled(LandingPageItem)<PProject>`
   height: 100vh;
   width: 100vw;
   z-index: 1;
-  opacity: ${(props: { isclicked: boolean }) => props.isclicked ? 0 : 1};
+  opacity: ${props => props.isclicked ? 0 : 1};
 
   @media only screen and (max-width: 700px) {
     display: none;
